04715728ab7fb75ec55236723f814e2e3cdf9409
Talisson Pedro <talissoncarmo65@gmail.com>
2025-03-19 17:33:18 -0300

    botão plus a function

--- app/src/main/java/com/example/projeto_01/MainActivity.kt
+++ app/src/main/java/com/example/projeto_01/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.projeto_01
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun sortear(view: View) {
+        val TextResultado = findViewById<TextView>(R.id.textResultado)
+        val numero = Random.nextInt(until = 10)
+        TextResultado.setText("Numero sorteado " + numero)
+
     }
 }
